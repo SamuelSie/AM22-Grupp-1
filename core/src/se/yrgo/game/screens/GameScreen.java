f80a7ec5f8b98378494abb1fa2ec3893fc53181f
Samuel Siesjo <samsie0717@skola.goteborg.se>
2023-02-23 08:54:16 +0100

    b

--- core/src/se/yrgo/game/screens/GameScreen.java
+++ core/src/se/yrgo/game/screens/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import se.yrgo.game.JumpyBirb;
@@ -27,6 +28,7 @@ public class GameScreen implements Screen {
     //Skapa sedan objekten i konstruktorn
     private BottomPipe bottomPipe;
     private TopPipe topPipe;
+    private GlyphLayout layout;
     
 
     public GameScreen(final JumpyBirb game) {
