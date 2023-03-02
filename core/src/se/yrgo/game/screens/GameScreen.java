7837e56e1cfe7cd0f17338024ac9b3644d6f959b
Samuel Siesjo <samsie0717@skola.goteborg.se>
2023-03-02 11:10:24 +0100

    refactored looping method in gamescreen, and all submethods in that method

--- core/src/se/yrgo/game/screens/GameScreen.java
+++ core/src/se/yrgo/game/screens/GameScreen.java
@@ -5,26 +5,21 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 import se.yrgo.game.JumpyBirb;
 import se.yrgo.game.objects.*;
 import se.yrgo.game.utils.Score;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GameScreen implements Screen {
     private final JumpyBirb game;
     private Doge doge;
-    
+
     private Ground ground;
-    
+
     private Music music;
     private OrthographicCamera camera;
     private float deltaTime;
@@ -35,50 +30,44 @@ public class GameScreen implements Screen {
     private long lastSpawnTime;
     private boolean isDead;
     private Score score;
-    
+
     public GameScreen(final JumpyBirb game, Score score) {
         this.game = game;
         //create doge & ground object with x & y position
         doge = new Doge(20, game.CAMY / 2);
         ground = new Ground(0, -50);
-        
+
         // background music
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
         music.setLooping(true);
-        
+
         // create camera
         camera = new OrthographicCamera();
         camera.setToOrtho(false, game.CAMX, game.CAMY);
-        
+
         // deltatime är tiden mellan frames, mätt i sekunder.
         //behövs för att flytta saker på skärmen, typ falla och hoppa.
         deltaTime = Gdx.graphics.getDeltaTime();
-        
+
         //Array av topPipes
         pipeArray = new Array<Pipe>();
-        
+
         spawnPipes();
         isDead = false;
 
         this.score = score;
     }
-    
-    private void spawnPipes() {
-        int isAdding = ThreadLocalRandom.current().nextInt(2);
-        int middleSpace = ThreadLocalRandom.current().nextInt(Pipe.getDISTANCE());
-        Pipe pipe = new Pipe(game.CAMX, game.CAMY / 2 - game.CAMY + (isAdding==1? middleSpace/2 : -middleSpace/2));
-        pipeArray.add(pipe);
-        lastSpawnTime = TimeUtils.nanoTime();
-    }
-    
+
+
+
     @Override
     public void render(float delta) {
-        
+
         //ScreenUtils.clear(0.2f,0.2f,0.2f,1);
-        
+
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
-        
+
         game.batch.begin();
         game.batch.draw(game.backGround, 0, 0, game.CAMX, game.CAMY);
         game.batch.draw(doge.getTexture(), doge.getPosition().x, doge.getPosition().y, doge.getTexture().getWidth(), doge.getTexture().getHeight());
@@ -93,48 +82,28 @@ public class GameScreen implements Screen {
         game.batch.end();
 
         //spawn pipes in the given time
-        if(TimeUtils.nanoTime() - lastSpawnTime > 3000000000L) spawnPipes();
+        if (TimeUtils.nanoTime() - lastSpawnTime > 3000000000L) spawnPipes();
 
         //loop over pipe conditions
-        for (Iterator<Pipe> iter = pipeArray.iterator(); iter.hasNext();) {
-            Pipe pipe = iter.next();
-            pipe.move();
-
-            //remove pipe if it gets to the edge
-            if (pipe.getPositionTop().x + pipe.getHitBoxTop().getWidth() < 0
-                || pipe.getPositionBottom().x + pipe.getHitBoxBottom().getWidth() < 0) {
-                iter.remove();
-            }
-
-            // set to Dead if doge collides with a pipe
-            if(doge.isCollided(pipe.getHitBoxTop()) || doge.isCollided(pipe.getHitBoxBottom())) {
-                isDead = true;
-            }
-
-            // get score if doge passes a pipe
-            if ((pipe.getPositionBottom().x + pipe.getHitBoxBottom().x) < doge.getPosition().x && !pipe.isScored()) {
-                score.score();
-                pipe.setScored(true);
-            }
-        }
-            //scoring
+        loopOverPipes();
+        //scoring
 //        }
-        
+
         //gör att doge faller nedåt
         doge.fall(deltaTime);
-        
+
         //ändrar hastigheten på doge y-axis från minus till plus
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) || Gdx.input.isTouched()) {
             doge.jump(deltaTime);
         }
 
-        
+
         if (doge.getPosition().y >= (game.CAMY - 60)) {
             doge.getPosition().y = (game.CAMY - 60);
             doge.resetVelocity();
         }
 
-        if(doge.isCollided(ground.getGroundBox())) isDead = true;
+
         // if doge is dead, switch to DeathScreen
         if (isDead) {
             score.newHighScore();
@@ -143,40 +112,84 @@ public class GameScreen implements Screen {
         }
 
         score.getLayout().setText(game.font, score.scoreToString());
-        
+
     }
-    
+
     @Override
     public void show() {
         //music.play();
         score.resetScore();
 
     }
-    
+
     @Override
     public void resize(int width, int height) {
-    
+
     }
-    
+
     @Override
     public void pause() {
-    
+
     }
-    
+
     @Override
     public void resume() {
-    
+
     }
-    
+
     @Override
     public void hide() {
-    
+
     }
-    
+
     @Override
     public void dispose() {
         music.dispose();
         doge.dispose();
         ground.dispose();
+    }
+
+    private void spawnPipes() {
+        int isAdding = ThreadLocalRandom.current().nextInt(2);
+        int middleSpace = ThreadLocalRandom.current().nextInt(Pipe.getDISTANCE());
+        Pipe pipe = new Pipe(game.CAMX, game.CAMY / 2 - game.CAMY + (isAdding == 1 ? middleSpace / 2 : -middleSpace / 2));
+        pipeArray.add(pipe);
+        lastSpawnTime = TimeUtils.nanoTime();
+    }
+
+    private void loopOverPipes() {
+        for (Iterator<Pipe> iter = pipeArray.iterator(); iter.hasNext(); ) {
+            Pipe pipe = iter.next();
+
+            pipe.move();
+
+            removePipe(iter, pipe);
+
+            checkCollision(pipe, ground);
+
+            updateScore(pipe);
+        }
+    }
+
+    private void checkCollision(Pipe pipe, Ground ground) {
+        if (doge.isCollided(pipe.getHitBoxTop()) || doge.isCollided(pipe.getHitBoxBottom()) ||
+                doge.isCollided(ground.getGroundBox())) {
+            isDead = true;
+        }
+    }
+
+    
+    private static void removePipe(Iterator<Pipe> iter, Pipe pipe) {
+        if (pipe.getPositionTop().x + pipe.getHitBoxTop().getWidth() < 0
+                || pipe.getPositionBottom().x + pipe.getHitBoxBottom().getWidth() < 0) {
+            iter.remove();
+        }
+    }
+
+    private void updateScore(Pipe pipe) {
+        if ((pipe.getPositionBottom().x + pipe.getHitBoxBottom().x) < doge.getPosition().x && !pipe.isScored()) {
+            score.score();
+            pipe.setScored(true);
+        }
     }
 }
