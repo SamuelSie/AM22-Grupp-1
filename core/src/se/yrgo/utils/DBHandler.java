4ec30641ee75ea693760493c02ec41eed877ce89
Adrian Åsling Sellius <adrian@gmail.com>
2023-03-23 10:41:29 +0100

    creating getTop5-method in DBHandler

--- core/src/se/yrgo/utils/DBHandler.java
+++ core/src/se/yrgo/utils/DBHandler.java
@@ -1,19 +1,49 @@
 package se.yrgo.utils;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBHandler {
-    public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:core/src/se/yrgo/game/utils/highscore.db")) {
+    private String connectionString;
+
+
+    public DBHandler() {
+        connectionString = "jdbc:sqlite:core/src/se/yrgo/database/highscore.db";
+    }
+
+    // hantera exception i Score
+    public List<String> getTop5Highscore() throws SQLException {
+        List<String> top5 = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(connectionString)) {
             Statement stm = conn.createStatement();
-            ResultSet rs = stm.executeQuery("select * from hoho");
+            ResultSet rs = stm.executeQuery("SELECT score FROM highscore ORDER BY score LIMIT 5");
 
             while (rs.next()) {
-                System.out.println(rs.getString("popo"));
+                top5.add(rs.getString("score"));
             }
-    }
-        catch (SQLException e) {
-            e.printStackTrace();
+
+            return top5;
         }
 
+//
+//    public void putHighscore(int score) {
+//
+//    }
+//    public static void main(String[] args) {
+//        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:core/src/se/yrgo/game/utils/highscore.db")) {
+//            Statement stm = conn.createStatement();
+//            ResultSet rs = stm.executeQuery("select * from hoho");
+//
+//            while (rs.next()) {
+//                System.out.println(rs.getString("popo"));
+//            }
+//    }
+//        catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
     }
 }
