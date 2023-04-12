6fd951e59bffc6ed7a3106751d943e0845e3fc35
Samuel Siesjo <samsie0717@skola.goteborg.se>
2023-04-12 22:37:06 +0200

    finally added idledoge with animation to mainmenuscreen, its messy but i will refactor

--- core/src/se/yrgo/game/sprites/IdleDoge.java
+++ core/src/se/yrgo/game/sprites/IdleDoge.java
@@ -1,16 +1,32 @@
 package se.yrgo.game.sprites;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import se.yrgo.utils.Animation;
 
-public class IdleDoge {
+public class IdleDoge extends Actor {
     private Animation animation;
     private Texture dogeRegion;
     
     public IdleDoge(){
         dogeRegion = new Texture("dogeIdle.png");
         animation = new Animation(new TextureRegion(dogeRegion),2,1f);
+    }
+
+    @Override
+    public void act (float delta) {
+        super.act(delta);
+        animation.update(delta);
+
+    }
+    @Override
+    public void draw (Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.draw(animation.getFrame(), 180, 70);
     }
     public void update(float delta){
         animation.update(delta);
